2d18608059565d118f12dbc7477ff1a99f725892
liana137403 <luoyalan47@gmail.com>
2026-05-07 19:19:10 +0800

    Delete locustfile.py

--- locustfile.py
+++
@@ -1,7 +0,0 @@
-from locust import HttpUser, task
-
-class TestUser(HttpUser):
-    host = "https://jsonplaceholder.typicode.com"
-    @task
-    def get_user(self):
-        self.client.get("/users/1")
