c9ac3e04e0e5aa11fa34750195ef9cfb59af7615
liana137403 <luoyalan47@gmail.com>
2026-05-07 19:19:01 +0800

    Delete llm_client.py

--- llm_client.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-def get_llm():
-    return ChatOpenAI(
-        api_key="sk-381890b52fe2450d90a8a1131373ea2e",
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-        model="qwen-turbo",
-        temperature=0.1
-    )
